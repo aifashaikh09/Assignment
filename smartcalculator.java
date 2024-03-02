7c082c061a59c99276daa6dd0f5aab932f66f654
unknown <aifashaikh1234@gmail.com>
2024-03-02 16:27:27 +0530

    Aifa- Commited java file

--- smartcalculator.java
+++ smartcalculator.java
@@ -1,3 +1,6 @@
+/**
+ * Calculates different mathematical problems
+ */
 package isa;
 
 public class smartcalculator 
@@ -9,6 +12,9 @@ public class smartcalculator
     static int fact =1 ;
 	private static int total, obtained;
 	
+	/**
+	 * Non parameterised constructor
+	 */
 	public smartcalculator ()
 	{
 		fact=1;
@@ -22,6 +28,13 @@ public class smartcalculator
 		time=2;
 	}
 	
+	/**
+	 * Parameterised constructor
+	 * @param a
+	 * @param time
+	 * @param rate
+	 * @param m
+	 */
 	public smartcalculator(int a, int time, int rate, int[] m) 
 	{
 		
@@ -30,39 +43,74 @@ public class smartcalculator
 		this.rate = rate;
 		this.m = m;
 	}
+	/**
+	 * getter method
+	 * @return total marks
+	 */
 
 	public static int getP() {
 		return p;
 	}
-
+/**
+ * This is Setter method 
+ * @param p
+ */
 	public static void setP(int p) {
 		smartcalculator.p = p;
 	}
 
+	/**
+	 * This is getter method
+	 * @return t
+	 */
 	public static int getT() {
 		return t;
 	}
 
+	/**
+	 * This is setter method
+	 * @param t
+	 */
 	public static void setT(int t) {
 		smartcalculator.t = t;
 	}
 
+	/**
+	 * This is getter method
+	 * @return r
+	 */
 	public static int getR() {
 		return r;
 	}
 
+	/**
+	 * This is setter method
+	 * @param r
+	 */
 	public static void setR(int r) {
 		smartcalculator.r = r;
 	}
 
+	/** 
+	 * This is getter method
+	 * @return total
+	 */
 	public static int getTotal() {
 		return total;
 	}
 
+	/**
+	 * This is setter method
+	 * @param total
+	 */
 	public static void setTotal(int total) {
 		smartcalculator.total = total;
 	}
 
+	/**
+	 * This is getter method
+	 * @return obtained
+	 */
 	public static int getObtained() {
 		return obtained;
 	}
@@ -70,11 +118,24 @@ public class smartcalculator
 	public static void setObtained(int obtained) {
 		smartcalculator.obtained = obtained;
 	}
+	
+	/**
+	 * Calculate simple interest
+	 * @return simple interest
+	 */
 
 	public static double simpleinterest()
 	{
 		return (p*t*r)/100;
 	}
+	
+	/**
+	 * Calculate compound interest
+	 * @param principal
+	 * @param rate
+	 * @param time
+	 * @return compound interest
+	 */
 	
 	public static double compoundinterest(int principal, int rate, int time)
 	{
@@ -82,6 +143,12 @@ public class smartcalculator
 		return (principal*Math.pow((1+rate/4),4*time));
 		
 	}
+	
+	/**
+	 * calculate of a given number
+	 * @param input number
+	 * @return mean of a number
+	 */
      
 	public  static int mean(int[] input)
 	{
@@ -91,6 +158,12 @@ public class smartcalculator
 	    }
 	    return sum / input.length;
 	}
+	
+	/**
+	 * Calculate factorial of a given number
+	 * @param num number
+	 * @return fact factorial of given number
+	 */
 	
 	public static int factorial(int num)
 	{
@@ -102,7 +175,12 @@ public class smartcalculator
 		return fact;
 	}
 	
-	public static double percent(int total, int obtained)
+	/**
+	 * Calculate percentage 
+	 * @return percentage
+	 */
+	
+	public static double percent()
 	{
 		return (obtained*100)/total;
 	}
